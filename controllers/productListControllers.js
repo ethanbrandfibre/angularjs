e6aa6b9a0e05f33a39611a5358d0510dfcdc5c89
ethan <ethan@brandfibre.com>
2014-07-13 16:30:33 +1200

    chagpter 11

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -3,15 +3,30 @@
  */
 angular.module('sportsStore')
     .constant("productListActiveClass","btn-primary")
-.controller("productListCtrl",function($scope, $filter){
+    .constant("productListPageCount",3)
+.controller("productListCtrl",function($scope, $filter, productListActiveClass,productListPageCount, cart){
        var selectedCategory = null;
+
+        $scope.selectedPage=1;
+        $scope.pageSize = productListPageCount;
+
         $scope.selectCategory = function(newCategory){
             selectedCategory = newCategory;
+            $scope.selectedPage =1;
+        };
+        $scope.selectPage=function(newPage) {
+            $scope.selectedPage = newPage;
         };
         $scope.categoryFilterFn = function(product){
             return selectedCategory == null || product.category == selectedCategory;
         };
-        $scope.getCategoryClass=function() {
-            return selectedCategory == category?productListActiveClass:"";
+        $scope.getCategoryClass=function(page) {
+            return $scope.selectedPage == page?productListActiveClass:"";
+        };
+        $scope.addProductToCart = function(product) {
+            cart.addProduct(product.id, product.name, product.price);
         };
     });
+
+//问题1： $scope里的变量和普通变量有什么区别
+//2. 函数里面的参数是如何对应到调用他们的html里面的函数的参数的？
